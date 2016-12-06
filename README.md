9a39ff44db518a97ccc752f487c733fdc1a56b38
Victor Coria <victorhcz@gmail.com>
2016-12-06 07:46:26 -0400

    Primera Version

---
+++ README.md
@@ -0,0 +1,10 @@
+TAREA 01 - Ejemplo Vistas (Views)
+=================================
+
+Tarea 01: ejemplo de uso de vistas, aplicación simulada Facebook. 
+* Study Jam II 2016
+* GDG Android Bolivia
+
+
+![Demo Aplicacion](/img/Captura00.png)
+
---
+++ app/src/main/java/com/example/victorhugo/studyjam_tarea_1/Activity01.java
@@ -0,0 +1,13 @@
+package com.example.victorhugo.studyjam_tarea_1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity01 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_01);
+    }
+}
